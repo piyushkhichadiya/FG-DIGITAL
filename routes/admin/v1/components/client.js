9a50aaff33acc7610e9c7114f93f4065ab4ae4ee
jenil <jenilnarola@gmail.com>
2020-07-15 03:38:33 -0400

    Client API update

--- routes/admin/v1/components/client.js
+++ routes/admin/v1/components/client.js
@@ -11,21 +11,21 @@ const client = require('express').Router(),
 
 //--------------------------------- ROUTES ---------------------------------
 
-//------------------------------- 3. CLIENT -------------------------------
+//------------------------------- 4. CLIENT -------------------------------
 
-//3.1 CREATE CLIENT ID
+//4.1 CREATE CLIENT ID
 client.post('/create', async(req, res) => {
     if (!req.body.name || !req.body.email || !req.body.password) {
-        return response(res, 400, 'Body required', 'name,email or password missing')
+        return response(res, 400, 'Body required', 'name,email or password missing', undefined, 'A-4.1.1')
     }
     var name = String(req.body.name),
         email = String(req.body.email),
         password = String(req.body.password)
     if (!regex.text(name)) {
-        return response(res, 400, 'Invalid', 'Name invalid')
+        return response(res, 400, 'Invalid', 'Name invalid', undefined, 'A-4.1.2')
     }
     if (!regex.email(email)) {
-        return response(res, 400, 'Invalid', 'Email value is invalid')
+        return response(res, 400, 'Invalid', 'Email value is invalid', undefined, 'A-4.1.3')
     }
     var pushData = {
         name: name.trim(),
@@ -40,114 +40,96 @@ client.post('/create', async(req, res) => {
                 clientKey = Object.keys(clientDB)
             for (var i = 0; i < clientKey.length; i++) {
                 if (clientDB[clientKey[i]].email == email) {
-                    return response(res, 403, 'Forbidden', 'Client With This Email Is Already Exist')
+                    return response(res, 403, 'Forbidden', 'Client With This Email Is Already Exist', , undefined, 'A-4.1.4')
                 } else if (i == clientKey.length - 1) {
                     firebase.database().ref('/admin/clients/').push(pushData)
-                    return response(res, 200, 'success', 'User created successfully')
+                    return response(res, 200, 'success', 'User created successfully', undefined, 'A-4.1.5')
                 }
             }
         }
     })
-
-
-
 
 })
 
-//3.2 Profile Update
-/**
- * -----profile update-----
-name
-update mail
-client mail
-client plan 
-payment 
-start time
-day time 
-
- */
+//4.2 Profile Update
 client.post('/update', async(req, res) => {
-        var pushData = {},
-            snapshot = await firebase.database().ref('/admin/clients/').once('value').then((snapshot) => {
-                if (snapshot.exists()) {
-                    return snapshot.val()
-                }
-            }),
-            clientDB = snapshot,
-            clientKey = Object.keys(clientDB)
-        if (!req.body.clientID) {
-            return response(res, 400, 'Invalid', 'Invalid Client Key')
-
+    var pushData = {},
+        snapshot = await firebase.database().ref('/admin/clients/').once('value').then((snapshot) => {
+            if (snapshot.exists()) {
+                return snapshot.val()
+            }
+        }),
+        clientDB = snapshot,
+        clientKey = Object.keys(clientDB)
+    if (!req.body.clientID) {
+        return response(res, 400, 'Invalid', 'Invalid Client Key', undefined, 'A-4.1.6')
+    }
+    var clientID = String(req.body.clientID).trim()
+    if (snapshot) {
+        var clientID = req.body.clientID
+        if (!clientKey.includes(clientID)) {
+            return response(res, 400, 'Invalid', 'Invalid Client Key', undefined, 'A-4.1.7')
         }
-        var clientID = String(req.body.clientID).trim()
+    }
+
+    if (req.body.name) {
+        var name = String(req.body.name)
+        if (!regex.text(name)) {
+            return response(res, 400, 'Invalid', 'Name invalid', undefined, 'A-4.1.8')
+        }
+        pushData.name = name
+    }
+    if (req.body.email) {
+        var email = String(req.body.email)
+        if (!regex.email(email)) {
+            return response(res, 400, 'Invalid', 'Email Invalid', undefined, 'A-4.1.9')
+        }
+
         if (snapshot) {
-            var clientID = req.body.clientID
-            if (!clientKey.includes(clientID)) {
-                return response(res, 400, 'Invalid', 'Invalid Client Key')
-            }
-        }
 
-        if (req.body.name) {
-            var name = String(req.body.name)
-            if (!regex.text(name)) {
-                return response(res, 400, 'Invalid', 'Name invalid')
-            }
-            pushData.name = name
-        }
-        if (req.body.email) {
-            var email = String(req.body.email)
-            if (!regex.email(email)) {
-                return response(res, 400, 'Invalid', 'Email Invalid')
-            }
-
-            if (snapshot) {
-
-                for (var i = 0; i < clientKey.length; i++) {
-                    if (clientDB[clientKey[i]].email == email && !clientDB[clientKey[i]].deleted) {
-                        return response(res, 403, 'Forbidden', 'Client With This Email Is Already Exist')
-                    } else if (i == clientKey.length - 1) {
-                        pushData.email = email
-                    }
+            for (var i = 0; i < clientKey.length; i++) {
+                if (clientDB[clientKey[i]].email == email && !clientDB[clientKey[i]].deleted) {
+                    return response(res, 403, 'Forbidden', 'Client With This Email Is Already Exist', undefined, 'A-4.1.10')
+                } else if (i == clientKey.length - 1) {
+                    pushData.email = email
                 }
             }
         }
-
-        if (req.body.plan) {
-            var plan = String(req.body.plan)
-            pushData.plan = plan
-
+    }
+    if (req.body.plan) {
+        var plan = String(req.body.plan)
+        pushData.plan = plan
+    }
+    if (req.body.price) {
+        var price = String(req.body.price)
+        if (!regex.regexNumber(price)) {
+            return response(res, 400, 'Invalid', 'Price Value Invalid', undefined, 'A-4.1.11')
         }
-        if (req.body.price) {
-            var price = String(req.body.price)
-            if (!regex.regexNumber(price)) {
-                return response(res, 400, 'Invalid', 'Price Value Invalid')
-            }
-            pushData.price = price
-
+        pushData.price = price
+    }
+    if (req.body.planStartDate) {
+        var startDate = String(req.body.planStartDate)
+        pushData.startDate = startDate
+    }
+    if (req.body.duration) {
+        var duration = String(req.body.duration)
+        pushData.duration = duration
+        if (!regex.regexNumber(duration)) {
+            return response(res, 400, 'Invalid', 'Duration Value Invalid', undefined, 'A-4.1.12')
         }
-        if (req.body.planStartDate) {
-            var startDate = String(req.body.planStartDate)
-            pushData.startDate = startDate
+    }
+    if (pushData) {
+        pushData.lastModifiedOn = String(new Date())
+        pushData.lastModifiedBy = "Admin"
 
-        }
-        if (req.body.duration) {
-            var duration = String(req.body.duration)
-            pushData.duration = duration
-            if (!regex.regexNumber(duration)) {
-                return response(res, 400, 'Invalid', 'Duration Value Invalid')
-            }
-        }
-        if (pushData) {
-            pushData.lastModifiedOn = String(new Date())
-            pushData.lastModifiedBy = "Admin"
+        firebase.database().ref(`/admin/clients/${clientID}/`).update(pushData).then(() => {
+            return response(res, 200, 'success', 'Profile Updated Successfully', undefined, 'A-4.1.13')
 
-            firebase.database().ref(`/admin/clients/${clientID}/`).update(pushData).then(() => {
-                return response(res, 200, 'success', 'Profile Updated Successfully')
+        })
+    }
+});
 
-            })
-        }
-    })
-    //3.3 DELETE CLIENT
+//4.3 DELETE CLIENT
 client.post('/delete', async(req, res) => {
     var snapshot = await firebase.database().ref('/admin/clients/').once('value').then((snapshot) => {
             if (snapshot.exists()) {
@@ -157,22 +139,21 @@ client.post('/delete', async(req, res) => {
         clientDB = snapshot,
         clientKey = Object.keys(clientDB)
     if (!req.body.clientID) {
-        return response(res, 400, 'Invalid', 'Invalid Client Key')
+        return response(res, 400, 'Invalid', 'Invalid Client Key', undefined, 'A-4.1.14')
     }
     var clientID = String(req.body.clientID).trim()
     if (snapshot) {
         var clientID = req.body.clientID
         if (!clientKey.includes(clientID)) {
-            return response(res, 400, 'Invalid', 'Invalid Client Key')
+            return response(res, 400, 'Invalid', 'Invalid Client Key', undefined, 'A-4.1.15')
         }
         if (clientDB[clientID].deleted) {
-            return response(res, 403, 'Forbidden', 'Client Account is already deleted')
+            return response(res, 403, 'Forbidden', 'Client Account is already deleted', undefined, 'A-4.1.16')
 
         }
     }
-
     firebase.database().ref(`/admin/clients/${clientID}/`).update({ "deleted": true }).then(() => {
-        return response(res, 200, 'success', 'Profile Updated Successfully')
+        return response(res, 200, 'success', 'Profile Updated Successfully', undefined, 'A-4.1.17')
 
     })
 })
