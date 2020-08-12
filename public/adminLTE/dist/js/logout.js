7103aa49cfbd72e0e11e3b878c8718864c6b0969
piyushkhichadiya <68903771+piyushkhichadiya@users.noreply.github.com>
2020-08-12 18:01:39 +0530

    Logout,Plan-add,plan,profile(update password)

--- public/adminLTE/dist/js/logout.js
+++ public/adminLTE/dist/js/logout.js
@@ -1,3 +1,24 @@
 function logout(){
-	// code here
+	Swal.fire({
+	  title: 'Are you sure?',
+	  text: "You want to Signout this session!",
+	  icon: 'warning',
+	  showCancelButton: true,
+	  confirmButtonColor: '#3085d6',
+	  cancelButtonColor: '#d33',
+	  confirmButtonText: 'Signout'
+	}).then((result) => {
+		if (result.value) {
+		  $.ajax({ 
+		  	url: 'http://localhost/admin/v1/user/logout', 
+		  	success: function () {
+		  	 document.location = '/admin/login'; 
+		  	}, 
+		  	error: function () { 
+		  		alert('Logout failed'); 
+		  	} 
+		  }); 
+		  return false;
+		}
+	})
 }
