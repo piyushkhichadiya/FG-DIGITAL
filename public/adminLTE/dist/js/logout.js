a16b00aea086778bef4717b6f1e9daa91c2f88c9
piyushkhichadiya <68903771+piyushkhichadiya@users.noreply.github.com>
2020-08-13 18:38:43 +0530

    profile and Plan

--- public/adminLTE/dist/js/logout.js
+++ public/adminLTE/dist/js/logout.js
@@ -1,24 +1,34 @@
-function logout(){
+function logout(force){
+	if(force){
+return session_expire();
+	}
 	Swal.fire({
 	  title: 'Are you sure?',
-	  text: "You want to Signout this session!",
+	  text: "You have to login again in order to access the portal.",
 	  icon: 'warning',
 	  showCancelButton: true,
-	  confirmButtonColor: '#3085d6',
-	  cancelButtonColor: '#d33',
-	  confirmButtonText: 'Signout'
+	  confirmButtonText: 'Log out'
 	}).then((result) => {
 		if (result.value) {
-		  $.ajax({ 
-		  	url: 'http://localhost/admin/v1/user/logout', 
-		  	success: function () {
-		  	 document.location = '/admin/login'; 
-		  	}, 
-		  	error: function () { 
-		  		alert('Logout failed'); 
-		  	} 
-		  }); 
-		  return false;
+		   session_expire()
 		}
 	})
+}
+function session_expire(){
+	$.get({ 
+		  	url: '/admin/v1/user/logout', 
+		  	success: function () {
+		  	 window.location.reload()
+		  	}, 
+		  	error: function () {
+		  		error=error.responseJSON 
+		  		$(document).Toasts('create', {
+	                title: 'Error #'+error.code,
+	                autohide: true,
+	                delay: 5000,
+	                body: error.message,
+	                class: 'bg-danger',
+	            }); 
+		  	} 
+		  });
 }
