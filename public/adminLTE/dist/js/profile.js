a3989e4a5bba9c093ca3a3b674e209b008c633d4
piyushkhichadiya <68903771+piyushkhichadiya@users.noreply.github.com>
2020-08-28 11:43:05 +0530

    Given(Mandatory) Changes

--- public/adminLTE/dist/js/profile.js
+++ public/adminLTE/dist/js/profile.js
@@ -4,4 +4,32 @@ $.get('/admin/v1/profile', (data) => {
         $('#lbl_profileName').text(profileData.name);
         $('#img_profileImage').attr('src', `/admin/v1/profile/file/` + profileData.profile_image)
     }
+}).fail(error => {
+    error = error.responseJSON
+    if (error.status == 404) {
+        $.get({
+            url: '/admin/v1/user/logout',
+            success: function() {
+                window.location.reload()
+            },
+            error: function() {
+                error = error.responseJSON
+                $(document).Toasts('create', {
+                    title: 'Error #' + error.code,
+                    autohide: true,
+                    delay: 5000,
+                    body: error.message,
+                    class: 'bg-danger',
+                });
+            }
+        });
+    } else {
+        $(document).Toasts('create', {
+            title: 'Error!',
+            autohide: true,
+            delay: 5000,
+            body: 'Profile Not Found',
+            class: 'bg-danger',
+        });
+    }
 });
