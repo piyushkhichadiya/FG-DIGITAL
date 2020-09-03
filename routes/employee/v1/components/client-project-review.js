529f70df007fa083d27c5a1ab81ab6444daed6cf
Jenil <developer@brossardappdesign.com>
2020-09-03 16:38:51 -0400

    Update method

---
+++ routes/employee/v1/components/client-project-review.js
@@ -0,0 +1,10 @@
+const clientProjectAPI = require('express').Router()
+
+module.exports = clientProjectAPI;
+// APIs
+/**
+ * 3.2 Add Activity
+ * 3.3 Update Activity
+ * 3.4 Remove File
+ * 3.5 Remove Activity
+ */
