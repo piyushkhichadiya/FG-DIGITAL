958d32007aa337bf52e255084a458cb712f34a40
Smit Luvani <16mscit068@gmail.com>
2020-09-07 17:43:32 +0530

    UI View Routes Set

--- routes/client/view.route.js
+++ routes/client/view.route.js
@@ -1,23 +1,35 @@
 const express = require('express'),
-    adminView = express.Router(),
+    clientView = express.Router(),
     checkAuth = require('./checkAuth')
 
 //Set Static Directory
-adminView.use(express.static('public/adminLTE'))
+clientView.use(express.static('public/adminLTE'))
 
 // ---------- Trailing Slash Static Asset ---------- 
 
 // Basic
-var routes = ['/login/']
+var routes = ['/login/', '/profile/']
 
-adminView.use(routes, express.static('public/adminLTE'))
+// Project
+routes.push.apply(routes, ['/projects/', '/project/', '/project-activity/', '/project-review', '/project-review-view'])
+
+clientView.use(routes, express.static('public/adminLTE'))
 
 // ------------------ VALIDATE USER ------------------
-// adminView.use(/^(?!\/login).*/, checkAuth);
+clientView.use(/^(?!\/login).*/, checkAuth);
 
-//------------------------- ADMIN VIEW ROUTES ----------------------
+//------------------------- CLIENT VIEW ROUTES ----------------------
 
 // Login
-adminView.get('/login', (req, res) => { res.sendFile(process.cwd() + '/views/portal/client/login.html') });
+clientView.get('/login', (req, res) => { res.sendFile(process.cwd() + '/views/portal/client/login.html') });
 
-module.exports = adminView;
+// Profile
+clientView.get('/profile', (req, res) => { res.sendFile(process.cwd() + '/views/portal/client/profile.html') });
+
+// Project
+clientView.get(['/projects', '/project'], (req, res) => { res.sendFile(process.cwd() + '/views/portal/client/projects.html') });
+clientView.get('/project-activity', (req, res) => { res.sendFile(process.cwd() + '/views/portal/client/activity.html') });
+clientView.get('/project-review', (req, res) => { res.sendFile(process.cwd() + '/views/portal/client/review.html') });
+clientView.get('/project-review-view', (req, res) => { res.sendFile(process.cwd() + '/views/portal/client/review-view.html') });
+
+module.exports = clientView;
