37f7eb413dfadb83acc3639cab0c7aa0c9d2d126
Piyush Khichadiya <68903771+piyushkhichadiya@users.noreply.github.com>
2020-09-11 17:00:24 +0530

    Url Check and Error Handle

--- public/adminLTE/dist/js/client-project.js
+++ public/adminLTE/dist/js/client-project.js
@@ -6,6 +6,15 @@ if (client_id) {
         var name = data.data.name;
         $('#lbl_navClient').html(name)
         $('#lbl_navClient').attr('href', `/admin/client`)
+    }).fail(error => {
+        error = error.responseJSON
+        $(document).Toasts('create', {
+            title: 'Error #' + error.code,
+            autohide: true,
+            delay: 5000,
+            body: error.message,
+            class: 'bg-danger',
+        });
     });
 }
 if (plan_id) {
@@ -13,5 +22,14 @@ if (plan_id) {
         var name = data.data.info.project_name
         $('#lbl_navProjectId').html(`${name} (${plan_id})`);
         $('#lbl_navProjectId').attr('href', `/admin/client/project-dashboard?client_id=${client_id}&plan_id=${plan_id}`);
+    }).fail(error => {
+        error = error.responseJSON
+        $(document).Toasts('create', {
+            title: 'Error #' + error.code,
+            autohide: true,
+            delay: 5000,
+            body: error.message,
+            class: 'bg-danger',
+        });
     });
 }
