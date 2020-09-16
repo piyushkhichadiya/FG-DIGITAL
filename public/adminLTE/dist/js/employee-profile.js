28de2a03e27ba82e15d7b6b06a0e828131bc5f94
Piyush Khichadiya <68903771+piyushkhichadiya@users.noreply.github.com>
2020-09-16 18:18:45 +0530

    Login,Profile,Projects
    Login-Logout
    Profile (get,update,update-password)
    Projects (get Projects in list )
    button to go dashboard
    in dashboard info is done

---
+++ public/adminLTE/dist/js/employee-profile.js
@@ -0,0 +1,17 @@
+$.get('/employee/v1/profile', (data) => {
+    profileData = data.data
+    if (profileData) {
+        $('#lbl_profileName').text(profileData.name);
+        $('#img_profileImage').attr('src', `/employee/v1/profile/file/` + profileData.profile_image)
+    }
+}).fail(error => {
+    error = error.responseJSON
+    if (error.status == 404 || error.status == 401) {
+        logout(true)
+    }
+});
+
+// Table Error Disable
+if ($.fn.dataTable && $.fn.dataTable.ext && $.fn.dataTable.ext.errMode) {
+    $.fn.dataTable.ext.errMode = 'none';
+}
