abdbe15d01bb1c8621ce31d1acbbe2ca32a02d00
Piyush Khichadiya <68903771+piyushkhichadiya@users.noreply.github.com>
2020-09-21 11:38:05 +0530

    Client Portal
    Ui of Login , profile and projects

--- public/adminLTE/dist/js/client-profile.js
+++ public/adminLTE/dist/js/client-profile.js
@@ -3,7 +3,6 @@ $.get('/client/v1/profile', (data) => {
     profileData = data.data
     if (profileData) {
         $('#lbl_profileName').text(profileData.name);
-        $('#img_profileImage').attr('src', `/client/v1/profile/file/` + profileData.profile_image)
     }
 }).fail(error => {
     error = error.responseJSON
@@ -12,6 +11,15 @@ $.get('/client/v1/profile', (data) => {
     }
 });
 
+if (urlParam.has('plan_id')) {
+    $.getJSON(`/client/v1/project/?project_id=${urlParam.get('plan_id')}`, (result) => {
+        var projectData = result.data;
+        $('#link_navProjects').html(projectData.name || '' + ` (${projectData.project_id})`);
+        $('#link_navProjects').attr('href', `/client/project-dashboard/?plan_id=${urlParam.get('plan_id')}`);
+    }).fail(error => {
+        window.location.href = '/employee/projects'
+    });
+}
 
 // Table Error Disable
 if ($.fn.dataTable && $.fn.dataTable.ext && $.fn.dataTable.ext.errMode) {
