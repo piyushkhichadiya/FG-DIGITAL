f63f7a0868fbc05b0412c1e186c64b65e03478d2
Smit Luvani <16mscit068@gmail.com>
2020-09-24 13:43:50 +0530

    A+E+C v1.0.0 Milestone

--- routes/employee/v1/api.employee.js
+++ routes/employee/v1/api.employee.js
@@ -21,6 +21,17 @@ employeeAPI.use('/profile', require('./components/profile'));
 // 3. CLIENT PROJECT
 employeeAPI.use('/project', require('./components/client-project'));
 
+// VERSION INFORMATION
+employeeAPI.use('/version', (req, res) => {
+    return response(res, 200, 'success', undefined, {
+        current_version: "1.0",
+        previous_version: "1.0",
+        info: {
+            "P.Q": "Major.Minor/Patches"
+        }
+    }, 'E-VERSION_INFO')
+})
+
 // 400 UNKNOWN APIs
 employeeAPI.use('*', (req, res) => {
     return response(res, 400, 'unknownApi', 'The API that the request is calling is not recognized.', undefined, 'A-API');
